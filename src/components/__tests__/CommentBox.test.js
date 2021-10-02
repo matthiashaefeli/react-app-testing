3eb36e561059ad46013bf4cbd7c2b94174f9a569
matthiashaefeli <haefeli.matthias2gmail.com>
2021-10-02 17:07:55 -0500

    semicolon

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -6,13 +6,13 @@ let wrapped;
 
 beforeEach(() => {
   wrapped = mount(<CommentBox />);
-})
+});
 
 afterEach(() => {
   wrapped.unmount();
-})
+});
 
 it('has a text are and a button', () => {
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
-})
+});
